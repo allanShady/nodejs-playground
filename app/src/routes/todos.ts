151b02851c40e947827e20530b2d2df4de590b4f
allanShady <acamilo.ag@gmail.com>
2021-03-11 12:33:45 +0200

    Chore: organize app structure

--- app/src/routes/todos.ts
+++ app/src/routes/todos.ts
@@ -1,19 +1,23 @@
 import { Express, Router } from 'express'
 import { Todo } from '../models/Todo'
 
-
 const router = Router();
 
 let todos: Todo[] = []; 
+
+type RequestBody = { text: string }
+type RequestParams = { todoId: string }
 
 router.get('/', (req, res, next) => {
     res.status(200).json({todos})
 })
 
 router.post('/todo', (req, res, next) => {
+    const body = req.body as RequestBody
+
     const todo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text
+        text: body.text
     }
 
     todos.push(todo);
@@ -22,18 +26,21 @@ router.post('/todo', (req, res, next) => {
 })
 
 router.put('/todo/:todoId', (req, res, next) => {
-    const indexOfTodoToUpdate = todos.findIndex((todo) => todo.id == req.params.todoId)
+    const params = req.params as RequestParams
+    const indexOfTodoToUpdate = todos.findIndex((todo) => todo.id == params.todoId)
 
     if(indexOfTodoToUpdate < 0)
         res.status(404).json({message: 'could not find todo with provided id'})
 
-    todos[indexOfTodoToUpdate] = { id: todos[indexOfTodoToUpdate].id, text: req.body.text }
+    const body = req.body as RequestBody
+    todos[indexOfTodoToUpdate] = { id: todos[indexOfTodoToUpdate].id, text: body.text }
     
     res.status(200).json({message: 'updated todo', todo: todos[indexOfTodoToUpdate]})
 })
 
 router.delete('/todo/:todoId', (req, res, next) => {
-    todos = todos.filter(todo => todo.id !== req.params.todoId)
+    const params = req.params as RequestParams
+    todos = todos.filter(todo => todo.id !== params.todoId)
 
     res.status(200).json({message: 'todo updated', todos})
 })
